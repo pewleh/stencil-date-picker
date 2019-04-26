3b92adf9f70c9f629e99070db0c2f10cbf5c61d5
Paula Nowicka <paula.nowicka@yahoo.co.uk>
2019-04-26 19:56:40 +0100

    Added basic layout and functionality

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -9,24 +9,44 @@ import moment from 'moment';
 })
 export class AppRoot {
   // array of months using moment
+  now = new Date()
   months = moment.months();
   @State() month: string = '';
   @State() days: any = [];
+  @State() selectedDate: object = {};
 
+  // building the days in the month for each selected month
   daysInMonth = month => {
     const date = new Date(2019, month, 1);
     const days = [];
     while (date.getMonth() === month) {
-        days.push(new Date(date));
-        date.setDate(date.getDate() + 1);
+        days.push({
+          date: moment(new Date(date)).format('Do'),
+          day: moment(new Date(date)).format('dddd'),
+          month: moment().month(month).format("MMMM"),
+          year: '2019'
+        });
+        date.setDate(date.getDate() + 1); //builds an array of objects with d/m/y in them.
     }
     return days;
   }
 
-  handleChange = (e, selected) => {
+  handleChange = e => {
     // set state does not seem to exist, so am updating directly, not sure if this is ok
-    this[selected] = e.target.value
+    this.month = e.target.value
     this.days = this.daysInMonth(this.months.indexOf(e.target.value));
+    this.selectedDate = {} // wiping any selected day if the month is changed
+  }
+  //when a drop downs selected, handle change runs setting days to the result of the above function.
+  handleClick = day => {
+    this.selectedDate = day
+  }
+  //maping over "months" array to individually build a dropdown // selector.
+
+  // use component did load, to selected january at the start
+  componentDidLoad() {
+    this.days = this.daysInMonth(this.now.getMonth());
+    console.log(this.days)
   }
 
   render() {
@@ -37,12 +57,20 @@ export class AppRoot {
         </header>
 
         <main>
-          <select onChange={e => this.handleChange(e, 'month')}>
-            {this.months.map(month => <option value={month} key={month}>{month}</option>)}
+          <select onChange={this.handleChange} value={this.month || this.months[this.now.getMonth()]}>
+            {this.months.map(month => <option>{month}</option>)}
           </select>
-          {this.days.map(day => <p key={day}>{`${day}`}</p>)}
+          <div className="calender">
+            {this.days.map(day => (
+              <div className={`date-square ${this.selectedDate.date === day.date ? 'selected': ''}`} key={day.date} onClick={() => this.handleClick(day)}>{`${day.day} ${day.date}`}</div>)}
+          </div>
+          {this.selectedDate.day &&
+            <div>
+              Selected date: {this.selectedDate.date}/{this.selectedDate.month}/{this.selectedDate.year}
+            </div>
+          }
         </main>
       </div>
-    );
+    );//56 - If selected day matches the date of the square built by map, give class of selected and style.
   }
 }
